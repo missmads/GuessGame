249cbae3e362a32b2994b1b7838807ebe96e0ded
Mads Kildegaard Kristjansen <m.i.s.s.mads@hotmail.com>
2019-03-07 14:49:03 +0100

    Done

--- GuessGame/GuessGame/Program.cs
+++ GuessGame/GuessGame/Program.cs
@@ -10,6 +10,26 @@ namespace GuessGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to 'GUESSGAME'!");
+            Console.WriteLine("The Rules are simple. Guess the creater's number");
+            Console.WriteLine("Tip: its between 0 and 1000. Good luck");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "666":
+                        Console.WriteLine("Gratz!!! You guessed the number!");
+                        break;
+                    case "quit":
+                        Console.WriteLine("Bye");
+                        return;
+                    default:
+                        Console.WriteLine("Hehe wrong number ;)");
+                        break;
+                }
+            }
         }
     }
 }
